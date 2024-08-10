31b3ddc26549f2ec4f7e2e9084599048944aea0c
Gideon Daniels <gideon.daniels@safetyio.com>
2024-08-10 18:52:14 +0200

    added basic two inputs functionality

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,28 +1,79 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalculatorService } from './calculator.service';
+import { FormsModule } from '@angular/forms';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'calculator';
+  numbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  operations: {
+    name: string;
+    symbol: string;
+  }[];
+  inputs: number[] = [];
   result = '';
-  inputOne = 5;
-  inputTwo = 5;
+  display = '0';
+  operation = '';
 
-  constructor(private calculatorService: CalculatorService) {}
+  constructor(private calculatorService: CalculatorService) {
+    this.operations = [
+      {
+        name: 'add',
+        symbol: '+',
+      },
+      {
+        name: 'multiply',
+        symbol: 'x',
+      },
+      {
+        name: 'subtract',
+        symbol: '-',
+      },
+      {
+        name: 'divide',
+        symbol: '/',
+      },
+    ];
+  }
 
-  onCalculate(action: string) {
+  onCalculate() {
     this.calculatorService
-      .calculate(action, [this.inputOne, this.inputTwo])
+      .calculate(this.operation, this.inputs)
       .subscribe((data) => {
         console.log(data);
         this.result = data.result;
       });
+
+    this.reset();
+  }
+
+  setOperation(operation: string, display: string) {
+    if (this.inputs.length >= 2) {
+      return alert('only two inputs is allowed');
+    }
+    this.display += `${display} `;
+    this.operation = operation;
+  }
+
+  onNumber(number: number) {
+    if (this.display === '0') {
+      this.display = '';
+    }
+    this.display += `${number} `;
+    this.inputs.push(number);
+  }
+
+  reset() {
+    this.inputs = [];
+    this.display = '0';
+    this.operation = '';
   }
 }
