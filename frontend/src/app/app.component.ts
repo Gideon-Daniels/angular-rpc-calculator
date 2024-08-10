a61f86386dbbc2c522db0546a6cbdd5c95817242
Gideon Daniels <gideon.daniels@safetyio.com>
2024-08-10 15:31:38 +0200

    added serve

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalculatorService } from './calculator.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'calculator';
+  result = '';
+
+  constructor(private calculatorService: CalculatorService) {}
+
+  addNumbers() {
+    this.calculatorService.add('add', [1, 2]).subscribe((data) => {
+      console.log(data);
+      this.result = data.result;
+    });
+  }
 }
---
+++ frontend/src/app/calculator.service.ts
@@ -0,0 +1,22 @@
+// json-rpc.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CalculatorService {
+  constructor(private http: HttpClient) {}
+
+  add(method: string, params: any[]): Observable<any> {
+    const body = {
+      jsonrpc: '2.0',
+      method: method,
+      params: params,
+      id: 1,
+    };
+
+    return this.http.post('http://localhost:3000', body);
+  }
+}
